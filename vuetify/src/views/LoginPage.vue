dcf11beb7b18a2a000659f9008c0eb5723e673bd
Jainil Patel <jainil.tech@gmail.com>
2022-04-05 14:13:23 +0530

    Card created

---
+++ vuetify/src/views/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+ <v-app>
+  <Card />
+
+ </v-app>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+export default {
+    
+components: {
+    Card
+},
+}
+</script>
+
+<style>
+
+</style>
