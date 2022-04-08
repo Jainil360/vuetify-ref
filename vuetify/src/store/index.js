c41a0ccafd805f86dfd9a92bbb2c21eddb5f3380
Jainil Patel <jainil.tech@gmail.com>
2022-04-08 15:39:10 +0530

    Vuex example added!

--- vuetify/src/store/index.js
+++ vuetify/src/store/index.js
@@ -5,13 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    jokes: [],
+    
   },
   mutations: {
+    setJokes(state, jokes) {
+      state.jokes = jokes
+    }
   },
   actions: {
+
   },
   modules: {
+
   }
 })
+
+
+// this.$store.commit('setJokes', [
+//   {
+//     id: 1,
+//     joke: 'Chuck Norris can hear sign language.',
+//     categories: ['nerdy']
+//   },
+// ])
 
 
